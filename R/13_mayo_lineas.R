39e27e8ecb5fcc6a50fa8ef6e05b5408ba1f0012
Carlos Aguero <dev.aguero@gmail.com>
2020-05-13 14:34:33 +0000

    Agregar fuente de datos

--- R/13_mayo_lineas.R
+++ R/13_mayo_lineas.R
@@ -1,4 +1,5 @@
 
+
 # Dependencias ------------------------------------------------------------
 
 library(tidyverse)
@@ -10,30 +11,38 @@ Sys.setlocale("LC_TIME", "es_ES.utf8")
 # Lectura datos -----------------------------------------------------------
 # http://geovision.uned.ac.cr/oges/
 datos <- read_csv("data/covid_12_mayo_cr.csv",
-                  col_types = cols(FECHA = col_date(format = "%d/%m/%Y"))) %>% 
-  clean_names() %>% 
-  mutate(semana = floor_date(fecha,"week")) %>% 
+                  col_types = cols(FECHA = col_date(format = "%d/%m/%Y"))) %>%
+  clean_names() %>%
+  mutate(semana = floor_date(fecha, "week")) %>%
   drop_na(fecha)
 
 
+# Grafico -----------------------------------------------------------------
+
 ggplot(data = datos, mapping = aes(x = fecha)) +
-  geom_line(mapping = aes(y = positivos, color = "1Total Casos"), size = 1) +
-  geom_line(mapping = aes(y = activos, color = "2Activos"), size = 1) +
-  geom_line(mapping = aes(y = recuperados, color = "3Recuperados"), size = 1) +
-  scale_x_date(date_labels = "%B\n%d",date_breaks = "week") +
-  labs(title = "Costa Rica: Casos positivos, activos y recuperados de Covid19",
-       subtitle = "Datos actualizados al 12 de mayo del 2020",
-       color = "",
-       y = "Cantidad de casos",
-       x = "") +
+  geom_line(mapping = aes(y = positivos, color = "1Total Casos"),
+            size = 1) +
+  geom_line(mapping = aes(y = activos, color = "2Activos"),
+            size = 1) +
+  geom_line(mapping = aes(y = recuperados, color = "3Recuperados"),
+            size = 1) +
+  scale_x_date(date_labels = "%B\n%d", date_breaks = "week") +
+  labs(
+    title = "Costa Rica: Casos positivos, activos y recuperados de Covid19",
+    subtitle = "Datos actualizados al 12 de mayo del 2020",
+    color = "",
+    y = "Cantidad de casos",
+    x = "",
+    caption = "Fuente: http://geovision.uned.ac.cr/oges/"
+  ) +
   scale_color_manual(
-    values = c("#363636", "#eb4559","#639a67"),
+    values = c("#363636", "#eb4559", "#639a67"),
     guide = guide_legend(
       title.position = "top",
       label.position = "left",
       label.vjust = 0.6
     ),
-    labels = c("Total de casos","Activos","Recuperados")
+    labels = c("Total de casos", "Activos", "Recuperados")
   )  +
   theme_minimal() +
   theme(
